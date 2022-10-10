252234c5e1c80746f057a31608220318e61e9cc5
Sasa Davic <sasadavic.nis@gmail.com>
2022-10-10 19:51:05 +0200

    login page

---
+++ src/pages/login_page.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class login_page {
+
+	private WebDriver driver;
+	
+	public login_page(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public WebElement getEmailInput() {
+		return driver.findElement(By.id("email"));		
+	}
+	
+	public WebElement getPasswordInput() {
+		return driver.findElement(By.id("password"));		
+	}
+	
+	public WebElement getLoginButton() {
+		return driver.findElement(By.xpath("//*[type = 'submit']"));		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
